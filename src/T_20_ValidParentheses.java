5f8f0a2a36dd4d0179ccd0cb0ccdab79e2c8aa6f
starikivan <peter.v.kramar@gmail.com>
2021-02-24 20:55:04 +0200

    20. Valid Parentheses

---
+++ src/T_20_ValidParentheses.java
@@ -0,0 +1,56 @@
+//20. Valid Parentheses
+//        Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//        An input string is valid if:
+//
+//        Open brackets must be closed by the same type of brackets.
+//        Open brackets must be closed in the correct order.
+//
+//
+//        Example 1:
+//
+//        Input: s = "()"
+//        Output: true
+//        Example 2:
+//
+//        Input: s = "()[]{}"
+//        Output: true
+//        Example 3:
+//
+//        Input: s = "(]"
+//        Output: false
+//        Example 4:
+//
+//        Input: s = "([)]"
+//        Output: false
+//        Example 5:
+//
+//        Input: s = "{[]}"
+//        Output: true
+//
+//
+//        Constraints:
+//
+//        1 <= s.length <= 104
+//        s consists of parentheses only '()[]{}'.
+
+import java.util.Stack;
+
+public class T_20_ValidParentheses {
+    public boolean isValid(String s) {
+        if (s.isEmpty()) return true;
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            if (c == '(' ) {
+                stack.push(')');
+            }else if (c == '[') {
+                stack.push(']');
+            }else if (c == '{') {
+                stack.push('}');
+            }else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}

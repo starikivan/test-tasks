db95fcb79a271603909953301410459dec82b9e2
starikivan <peter.v.kramar@gmail.com>
2022-02-02 17:05:01 +0200

    codility 8. EquiLeader

---
+++ src/codility/C_08_EquiLeader.java
@@ -0,0 +1,86 @@
+/**
+        EquiLeader
+        Find the index S such that the leaders of the sequences A[0], A[1], ..., A[S] and A[S + 1], A[S + 2], ...,
+        A[N - 1] are the same.
+        Programming language:
+        Java 11
+        A non-empty array A consisting of N integers is given.
+
+        The leader of this array is the value that occurs in more than half of the elements of A.
+
+        An equi leader is an index S such that 0 ≤ S < N − 1 and two sequences A[0], A[1], ..., A[S] and A[S + 1],
+        A[S + 2], ..., A[N − 1] have leaders of the same value.
+
+        For example, given array A such that:
+
+        A[0] = 4
+        A[1] = 3
+        A[2] = 4
+        A[3] = 4
+        A[4] = 4
+        A[5] = 2
+        we can find two equi leaders:
+
+        0, because sequences: (4) and (3, 4, 4, 4, 2) have the same leader, whose value is 4.
+        2, because sequences: (4, 3, 4) and (4, 4, 2) have the same leader, whose value is 4.
+        The goal is to count the number of equi leaders.
+
+        Write a function:
+
+        class Solution { public int solution(int[] A); }
+
+        that, given a non-empty array A consisting of N integers, returns the number of equi leaders.
+
+        For example, given:
+
+        A[0] = 4
+        A[1] = 3
+        A[2] = 4
+        A[3] = 4
+        A[4] = 4
+        A[5] = 2
+        the function should return 2, as explained above.
+
+        Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..100,000];
+        each element of array A is an integer within the range [−1,000,000,000..1,000,000,000].
+*/
+
+package codility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C_08_EquiLeader {
+    public int solution(int[] A) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int dominator = -1;
+        for (int a : A) {
+            if (map.containsKey(a)) {
+                map.put(a, map.get(a) + 1);
+            } else {
+                map.put(a, 1);
+            }
+            if (dominator == -1 && map.get(a) > A.length / 2) {
+                dominator = a;
+            }
+        }
+        if (dominator == -1) {
+            return 0;
+        }
+        int equiLeaders = 0;
+        int dominatorsLeft = 0;
+        int dominatorsRight =map.get(dominator);
+        for (int i=0; i<A.length-1; i++) {
+            if (A[i] == dominator) {
+                dominatorsLeft++;
+                dominatorsRight--;
+            }
+            if (dominatorsLeft > (i + 1) / 2 && dominatorsRight > (A.length - i - 1) / 2) {
+                equiLeaders++;
+            }
+        }
+        return equiLeaders;
+    }
+}

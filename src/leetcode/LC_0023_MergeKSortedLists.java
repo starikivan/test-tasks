f8fecedc87f672bbc60d3cb48d981d5c35b72915
starikivan <peter.v.kramar@gmail.com>
2023-02-01 11:27:49 +0200

    leetcode 23. Merge k Sorted Lists

--- src/leetcode/LC_0023_MergeKSortedLists.java
+++ src/leetcode/LC_0023_MergeKSortedLists.java
@@ -80,20 +80,19 @@ public class LC_0023_MergeKSortedLists {
         }
         return temp.next;
     }
-}
 
-class ListNode {
-    int val;
-    ListNode next;
+    private static class ListNode {
+        int val;
+        ListNode next;
 
-    public ListNode() {}
+        public ListNode() {}
 
-    public ListNode(int val) {
-        this.val = val;
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
     }
-
-    public ListNode(int val, ListNode next) {
-        this.val = val; this.next = next;
-    }
 }
-

4ab1ed0adca629b79d7bed13a3650c922c4b36a6
petrokramar <peter.v.kramar@gmail.com>
2024-08-21 07:55:55 +0300

    leetcode 28. Find the Index of the First Occurrence in a String

---
+++ src/leetcode/LC_0028_FindIndexOfFirstOccurrenceInString.java
@@ -0,0 +1,44 @@
+/**
+28. Find the Index of the First Occurrence in a String
+Easy
+Given two strings needle and haystack, return the index of the first occurrence of needle in
+haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+*/
+
+package leetcode;
+
+public class LC_0028_FindIndexOfFirstOccurrenceInString {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if(haystack.charAt(i) == needle.charAt(0)) {
+                boolean found = true;
+                for (int j = 1; j < needle.length(); j ++) {
+                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
